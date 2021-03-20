41c612fdc1a67ce9ae848dfff0fe6000562b1468
DenisRayko <raykod96@gmail.com>
2021-03-21 01:32:50 +0700

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,32 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
+
+	// реализуйте здесь задание №1
+
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let avarageGrade = (algebra + geography + physics) / 3;
+
+	return avarageGrade;
 }
+// реализуйте здесь задание №2
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
+
+	return message;
 }
+// реализуйте здесь задание №3
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
+
+	return result;
 }
