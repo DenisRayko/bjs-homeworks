230cbef3788f240920abf33f8b6e17d177393675
DenisRayko <raykod96@gmail.com>
2021-04-07 19:07:11 +0700

    initial commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,39 @@
+"use strict";
+
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let today =  new Date();
+    let monthsQuantity = (date.getFullYear() - today.getFullYear()) * 12 - (today.getMonth() + 1) + date.getMonth() + 1;;
+    if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    }
+     
+    else if (isNaN(contribution)) {
+        return `Параметр "Первоначальный вхнос" содержит неправильное значение ${contribution}`;
+        }
+
+  else if (isNaN(amount)) {
+            return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
+            }
+
+    else if (date =='Invalid date'|| monthsQuantity < 12) {
+                return `Введите корректную дату погашения ипотеки`;
+                }
+else{
+let sumLeft = amount - contribution;
+let monthlyPercent = percent / 1200; 
+let monthlyPayment = sumLeft * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent)) - 1));
+let totalAmount = monthlyPayment * monthsQuantity; 
+ return totalAmount.toFixed(2);
+}
 }
 
-function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
-}
+    function getGreeting(name) {
+        if (!name || name == "null" || name == "undefined") {
+          name = "Анон";
+        }
+        let greeting = `Привет, мир! Меня зовут ${name}`;
+        return greeting;
+      }
